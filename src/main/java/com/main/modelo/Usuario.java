0337522c0b70fff1444d87423866063ac35b8ef8
AdamCampos <adam.x.campos@gmail.com>
2021-08-23 18:09:09 -0300

    Inserido Lombok

---
+++ src/main/java/com/main/modelo/Usuario.java
@@ -0,0 +1,15 @@
+package com.main.modelo;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor	
+public class Usuario {
+
+	private int matricula;
+	private String nome;
+	
+	
+	
+}
--- src/test/java/com/main/TesteControlador.java
+++ src/test/java/com/main/TesteControlador.java
@@ -6,12 +6,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import com.main.controlador.Controlador;
 
@@ -19,13 +22,20 @@ import com.main.controlador.Controlador;
 @WebMvcTest(Controlador.class)
 public class TesteControlador {
 
+	private static final Logger LOGGER = LogManager.getLogger();
+
 	@Autowired
 	private MockMvc mockMvc;
 
 	@Test
 	public void testHomePage() throws Exception {
-		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("home"))
-				.andExpect(content().string(containsString("Welcome to...")));
+
+		ResultActions ra1 = mockMvc.perform(get("/"));
+		ResultActions ra2 = ra1.andExpect(status().isOk());
+		ResultActions ra3 = ra2.andExpect(view().name("home"));
+		ResultActions ra4 = ra3.andExpect(content().string(containsString("Welcome to...")));
+
+		LOGGER.debug("RA " + ra4.toString());
 
 	}
 }
