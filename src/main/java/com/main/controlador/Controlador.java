b4b672897dc237fb8d1bbc33019c24585e26e506
AdamCampos <adam.x.campos@gmail.com>
2021-08-23 14:54:43 -0300

    Testado Springboot no Eclipse. Rodando no Tomcat

--- src/main/java/com/main/controlador/Controlador.java
+++ src/main/java/com/main/controlador/Controlador.java
@@ -1,5 +1,13 @@
 package com.main.controlador;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class Controlador {
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}	
 
 }
